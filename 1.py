9005108127289060711b0c2a268a46fc5a4ace4f
Insert <seregasmolnyi48@gmail.com>
2025-06-02 08:41:32 +0000

    modified:   1.py

--- 1.py
+++ 1.py
@@ -4,6 +4,3 @@ def get_sq(x):
 num = float(input())
 
 print(get_sq(num))
-
-
-
---
+++ 5.py
@@ -0,0 +1,6 @@
+def meow(m):
+    return m, len(m);
+
+cityINP = input().strip();
+city = cityINP.split();
+d = {gorod: meow(gorod)[1] for gorod in city};
