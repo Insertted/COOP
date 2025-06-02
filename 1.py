cf9e6fb4dd0d754c7ef2dcdcea31e4ffe0855af7
Insert <seregasmolnyi48@gmail.com>
2025-06-02 08:27:22 +0000

    new file:   1.py 	new file:   2.py 	new file:   3.py 	new file:   4.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+def get_sq(x):
+    return x ** 2
+
+num = float(input())
+
+print(get_sq(num))
+
+
+
---
+++ 3.py
@@ -0,0 +1,13 @@
+def is_even(n):
+    return n % 2 == 0
+
+
+
+while True:
+    x = int(input())
+    if x == 1:
+        break
+
+
+    if is_even(x):
+        print(x)
