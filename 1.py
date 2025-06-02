d3920e09fc92b7f20f4621f8e18fa6374e818489
Insert <seregasmolnyi48@gmail.com>
2025-06-02 09:17:09 +0000

    deleted:    1.py 	deleted:    2.py 	deleted:    3.py 	deleted:    4.py 	deleted:    5.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-def get_sq(x):
-    return x ** 2
-
-num = float(input())
-
-print(get_sq(num))
- 
--- 2.py
+++
@@ -1,7 +0,0 @@
-def is_triangle(a, b, c):
-    return a + b > c and a + c > b and b + c > a
-
-
-a = input();
-b = input();
-c = input();
