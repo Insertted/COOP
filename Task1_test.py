62d122fd8ad5d4557c2dadc1a12a0692dfe6986a
Insert <seregasmolnyi48@gmail.com>
2025-06-03 00:56:44 +0000

    renamed:    app/Task1_test.py -> Task1_test.py 	new file:   Task2_test.py 	new file:   app/__pycache__/task2.cpython-312.pyc

--- Task1_test.py
+++ Task1_test.py
@@ -1,5 +1,5 @@
 import pytest
-from task1 import get_sq
+from app.task1 import get_sq
 
 def testing():
     assert get_sq(2) == 4;
---
+++ Task2_test.py
@@ -0,0 +1,5 @@
+import pytest
+from app.task2 import is_triangle
+
+def testing():
+    assert is_triangle(2,5,5) == "True";
